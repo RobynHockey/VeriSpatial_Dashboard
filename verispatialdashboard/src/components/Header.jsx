b8a56ce5cc3e2ca5aa37bbe0b1f179fad47f8d06
RobynHockey <u22488244@tuks.co.za>
2025-10-17 20:02:50 +0200

    commit

---
+++ verispatialdashboard/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <img src="/logoverispatial.png" alt="Verispatial Logo" className="logo" />
+        <h1>Verispatial</h1>
+      </div>
+    </header>
+  );
+}
---
+++ verispatialdashboard/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function MainContent() {
+  return (
+    <main className="main-content">
+      <h2>Dashboard Content Coming Soon</h2>
+      <p>This is where your 3D model will be displayed later.</p>
+    </main>
+  );
+}
